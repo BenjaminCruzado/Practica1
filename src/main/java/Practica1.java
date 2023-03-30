60320cc6c440356778333f1b49b97e3b260e7e1d
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-03-29 21:01:48 -0300

    Subo el codigo

---
+++ src/main/java/Practica1.java
@@ -0,0 +1,12 @@
+public class Practica1 {
+    public static void main(String[] args){
+        hola();
+        elFinal();
+    }
+    public static void hola(){
+        System.out.println("Hola");
+    }
+    public static void elFinal(){
+        System.out.println("Final");
+    }
+}
